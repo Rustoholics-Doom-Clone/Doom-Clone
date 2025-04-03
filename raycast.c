b93f8126b5557e649efa7b27b323b5fa0505b86e
DanteNorlingTE21 <dantenorling@gmail.com>
2025-04-03 13:43:54 +0200

    Implemented vectorAdd and vectorScale

--- raycast.c
+++ raycast.c
@@ -8,8 +8,17 @@ void vectorSub(Vec2 v1, Vec2 v2, Vec2 *result)
     result->y = v1.y - v2.y;
 }
 
-void vectorAdd(Vec2 v1, Vec2 v2, Vec2 *result) {}
-void vectorScale(Vec2 v1, float k, Vec2 *result) {}
+void vectorAdd(Vec2 v1, Vec2 v2, Vec2 *result)
+{
+    result->x = v1.x + v2.x;
+    result->y = v1.y + v2.y;
+}
+
+void vectorScale(Vec2 v1, float k, Vec2 *result)
+{
+    result->x = v1.x * k;
+    result->y = v1.y * k;
+}
 void vectorDot(Vec2 v1, Vec2 v2, Vec2 *result) {}
 
 CollisionData *checkCollision(Wall w1, Ray r1)
