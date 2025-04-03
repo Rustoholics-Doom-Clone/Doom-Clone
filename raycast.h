37c7cb8a7fc08df9eca84fab6cafcf8a15737fdf
DanteNorlingTE21 <dantenorling@gmail.com>
2025-04-03 12:05:14 +0200

    Added CollisionData type

--- raycast.h
+++ raycast.h
@@ -7,4 +7,10 @@ typedef struct
     float y;
 } Vec2;
 
+typedef struct
+{
+    Vec2 position; // where the collision happened
+    float d;       // how far away it happened
+} CollisionData;
+
 #endif
