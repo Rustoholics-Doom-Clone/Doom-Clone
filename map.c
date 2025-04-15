3de3fdb51f62babdc00d172dcdcdae0465c556ac
DanteNorlingTE21 <dantenorling@gmail.com>
2025-04-15 20:38:03 +0200

    Implemented saveMap

--- map.c
+++ map.c
@@ -4,6 +4,18 @@
 #include <float.h>
 #include "map.h"
 
+int saveMap(int numOfWalls, Wall *walls, char *filename)
+{
+    FILE *mfile = fopen(filename, "w");
+    if (!mfile)
+        return 0;
+    fprintf(mfile, "%d,0\n", numOfWalls);
+    for (int i = 0; i < numOfWalls; i++)
+        fprintf(mfile, "%f,%f\n", walls[i].start, walls[i].stop);
+    fclose(mfile);
+    return 1;
+}
+
 void freeMap(Map *m)
 {
     if (m->walls)
