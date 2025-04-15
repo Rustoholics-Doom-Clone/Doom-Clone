7decef183ecd4e1bf89c4b807f6dcfe76dd09756
DanteNorlingTE21 <dantenorling@gmail.com>
2025-04-15 15:18:06 +0200

    Made Map struct

--- map.h
+++ map.h
@@ -2,4 +2,13 @@
 
 #ifndef MAPH
 #define MAPH
+
+typedef struct
+{
+    int numOfWalls;
+    Wall *walls;
+    int enemyCount; // Will most definetly be reworked when enemy logic is made
+    Vec2 *enemyPos; // Will most definetly be reworked when enemy logic is made
+} Map;
+
 #endif
