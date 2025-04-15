dea3534b4965b6313d97506964e2de19eb5e831f
DanteNorlingTE21 <dantenorling@gmail.com>
2025-04-15 20:54:23 +0200

    Implemented loadMap

--- map.c
+++ map.c
@@ -16,6 +16,41 @@ int saveMap(int numOfWalls, Wall *walls, char *filename)
     return 1;
 }
 
+Map *loadMap(char *filename)
+{
+    FILE *mfile = fopen(filename, "r");
+    if (!mfile)
+        return NULL;
+    Map *result = malloc(sizeof(Map));
+    if (!result)
+        return NULL;
+
+    char buffer[50];
+    if (!fgets(buffer, sizeof(buffer), mfile))
+    {
+        fclose(mfile);
+        free(result);
+        return NULL;
+    }
+    int nwalls, nenemy;
+    sscanf(buffer, "%d,%d", &nwalls, &nenemy);
+
+    result->walls = malloc(sizeof(Wall) * nwalls);
+    if (!result->walls)
+    {
+        fclose(mfile);
+        free(result);
+        return NULL;
+    }
+    // Enemies here. TODO!
+
+    for (int i = 0; i < nwalls && fgets(buffer, sizeof(buffer), mfile); i++)
+        sscanf(buffer, "%f,%f", &result->walls[i].start, &result->walls[i].stop);
+
+    fclose(mfile);
+    return result;
+}
+
 void freeMap(Map *m)
 {
     if (m->walls)
