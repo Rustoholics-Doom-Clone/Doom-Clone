f6c3aa6d73737cf134fb8c936ace8a60efb60b39
DanteNorlingTE21 <dantenorling@gmail.com>
2025-04-25 11:55:24 +0200

    Removed old pre-map stuff from main

--- main.c
+++ main.c
@@ -14,45 +14,6 @@
 #define NUM_RAYS 200
 #define FOV 60.0f
 
-int map[MAP_HEIGHT][MAP_WIDTH] = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 1, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
-
-int buildWallsFromMap(Wall *walls, int maxWalls)
-{
-    int count = 0;
-    for (int y = 0; y < MAP_HEIGHT; y++)
-    {
-        for (int x = 0; x < MAP_WIDTH; x++)
-        {
-            if (map[y][x] == 1)
-            {
-                Vec2 tl = {x * TILE_SIZE, y * TILE_SIZE};
-                Vec2 tr = {(x + 1) * TILE_SIZE, y * TILE_SIZE};
-                Vec2 bl = {x * TILE_SIZE, (y + 1) * TILE_SIZE};
-                Vec2 br = {(x + 1) * TILE_SIZE, (y + 1) * TILE_SIZE};
-
-                if (count + 4 < maxWalls)
-                {
-                    walls[count++] = (Wall){tl, tr};
-                    walls[count++] = (Wall){tr, br};
-                    walls[count++] = (Wall){br, bl};
-                    walls[count++] = (Wall){bl, tl};
-                }
-            }
-        }
-    }
-    return count;
-}
-
 void draw3DView(CollisionData **hits, int rayCount)
 {
     for (int i = 0; i < rayCount; i++)
